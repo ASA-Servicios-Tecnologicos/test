3d6a3e07df34d5b9e9553057ffc57fe4cdbf5513
Roberto Castellor <rcastellor@hiberus.com>
2021-12-14 15:47:53 +0100

    [FEAT] ficheros k8s

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppConfigModule } from '../configuration/configuration.module';
 import { Booking, BookingSchema } from '../shared/model/booking.schema';
+import { BookingController } from './booking.controller';
 @Module({
   imports: [
     AppConfigModule,
@@ -9,5 +10,6 @@ import { Booking, BookingSchema } from '../shared/model/booking.schema';
   ],
   providers: [],
   exports: [],
+  controllers: [BookingController],
 })
 export class BookingModule {}
