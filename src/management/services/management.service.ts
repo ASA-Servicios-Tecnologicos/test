45de41c0262a94ae592df7b435a7cf499e9b93da
Daniel Nieto Andrés <dnieto@hiberus.com>
2022-01-20 11:56:10 +0100

    Remove agent

--- src/management/services/management.service.ts
+++ src/management/services/management.service.ts
@@ -1,7 +1,6 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { lastValueFrom, pluck } from 'rxjs';
 import { AppConfigService } from '../../configuration/configuration.service';
-import * as https from 'https'
 @Injectable()
 export class ManagementService {
   constructor(
@@ -10,12 +9,12 @@ export class ManagementService {
   ) {
    
   }
-  agent = new https.Agent({rejectUnauthorized: false})
+
   auth() {
     const authData = new URLSearchParams();
     authData.append('username', this.appConfigService.MANAGEMENT_USERNAME);
     authData.append('password', this.appConfigService.MANAGEMENT_PASSWORD);
-    return lastValueFrom(this.http.post(`${this.appConfigService.MANAGEMENT_URL}/user/auth/token-auth/`, authData, { httpsAgent: this.agent}).pipe(pluck('data', 'token'))).catch(error => console.log(error));
+    return lastValueFrom(this.http.post(`${this.appConfigService.MANAGEMENT_URL}/user/auth/token-auth/`, authData).pipe(pluck('data', 'token'))).catch(error => console.log(error));
   }
   
 /*   async doCheckout(checkout: CheckoutDTO) {
