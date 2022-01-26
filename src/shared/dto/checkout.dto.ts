bda027c0c352c58fdfea8e76c20412d2483fcea2
Daniel Nieto Andrés <dnieto@hiberus.com>
2022-01-26 16:05:39 +0100

    add cancelattion policies checkout

--- src/shared/dto/checkout.dto.ts
+++ src/shared/dto/checkout.dto.ts
@@ -14,5 +14,6 @@ export interface BookingDTO {
   language: string;
   startDate: string;
   endDate: string;
+  cancellationPolicies: Array<CancellationPolicyDTO>;
   distribution: Array<DistributionDTO>;
 }
