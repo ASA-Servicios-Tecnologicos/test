929ebd0b26cf90ecce55547ef7bc490edc7d40b6
Sergio Pedrero Benedi <spedrero@hiberus.com>
2022-02-09 08:54:07 +0100

    ADD-AuthenticationUserMiddleware,client endpoints

--- src/middlewares/authenticacion-user.middleware.ts
+++ src/middlewares/authenticacion-user.middleware.ts
@@ -1,27 +1,27 @@
 import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { Request } from 'express';
 import * as authenticationService from 'tecnoturis-authentication';
+import t from 'typy';
 
 @Injectable()
 export class AuthenticationUserMiddleware implements NestMiddleware {
   constructor() {}
+
   async use(req: Request, res: any, next: () => void) {
-    const tokenFromHeader = req.headers.authorization;
-    console.log(
-      '🚀 ~ file: authenticacion-user.middleware.ts ~ line 10 ~ AuthenticationUserMiddleware ~ use ~ tokenFromHeader',
-      tokenFromHeader,
-    );
+    const tokenFromHeader: string = t(req, 'headers.authorization').safeObject;
     if (!tokenFromHeader) {
       throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
     }
+    const token = tokenFromHeader.replace('Bearer ', '');
     try {
-      const data = await authenticationService.verify('');
-      console.log('🚀 ~ file: authenticacion-user.middleware.ts ~ line 17 ~ AuthenticationUserMiddleware ~ use ~ data', data);
-
+      const data = await authenticationService.verify(token);
       if (!data) {
-        throw new HttpException('Internal error.', HttpStatus.INTERNAL_SERVER_ERROR);
+        throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
       }
-    } catch (e) {}
+    } catch (e) {
+      console.error(e);
+      throw new HttpException('Not authorized.', HttpStatus.UNAUTHORIZED);
+    }
     next();
   }
 }
