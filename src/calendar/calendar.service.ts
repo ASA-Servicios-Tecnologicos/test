95aff7079ce844c037f97c639a06f30d9c1ac48b
Sergio Pedrero Benedi <spedrero@hiberus.com>
2022-02-15 09:22:56 +0100

    WIP

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { AppConfigService } from '../configuration/configuration.service';
+import { ManagementHttpService } from '../management/services/management-http.service';
 
 @Injectable()
 export class CalendarService {
+  constructor(private readonly managementHttpService: ManagementHttpService, private readonly appConfigService: AppConfigService) {}
   getReferencePrices() {
     return this.managementHttpService.get(`${this.appConfigService.BASE_URL}/packages-newblue/api/v1/pre-bookings/${hash}/`);
   }
