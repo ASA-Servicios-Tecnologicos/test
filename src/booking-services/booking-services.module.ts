4b2e37aa720a681d753e63aae566323639f3f86f
Daniel Nieto Andrés <dnieto@hiberus.com>
2022-03-09 09:54:09 +0100

    ADD endpoint booking service by dossier id

--- src/booking-services/booking-services.module.ts
+++ src/booking-services/booking-services.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ManagementModule } from 'src/management/management.module';
 import { BookingServicesFlightsModule } from './booking-services-flights/booking-services-flights.module';
 import { BookingServicesHotelRoomsModule } from './booking-services-hotel-rooms/booking-services-hotel-rooms.module';
+import { BookingServicesController } from './booking-services.controller';
 
 @Module({
-  imports: [BookingServicesFlightsModule, BookingServicesHotelRoomsModule],
-  controllers: [],
+  imports: [BookingServicesFlightsModule, BookingServicesHotelRoomsModule, ManagementModule],
+  controllers: [BookingServicesController],
   providers: [],
   exports: [],
 })
