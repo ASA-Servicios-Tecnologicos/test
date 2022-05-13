e9dc6d3bf14020f4db2d9528851d557f875e678e
David Soriano <david.soriano@tecnoturis.es>
2022-05-13 11:21:35 +0200

    console log

--- src/management/services/client.service.ts
+++ src/management/services/client.service.ts
@@ -77,10 +77,12 @@ export class ClientService {
     }
     
     async AddToNewsletter(newsletterRequestDTO: { email: string }) {
+    console.log(newsletterRequestDTO);
     const response = await this.managementHttpService.post(
       `${this.appConfigService.BASE_URL}/management/api/v1/client/external/add-newsletter/`,
       newsletterRequestDTO,
     );
+    console.log(response);
     if (response !== "Este email ya esta suscrito a la newsletter") this.notificationService.sendNewsletterConfirmation(newsletterRequestDTO.email); 
   }
 
