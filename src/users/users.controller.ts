f94a463cfacf9d327370fd2a20874fd4fffbf47e
Osti Katriel <osti.barrozo.pe@cloudappi.net>
2022-06-07 12:28:51 -0500

    add catch

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, HttpException } from '@nestjs/common';
 import { LoginPayloadDTO } from '../shared/dto/login-payload.dto';
 import { UsersService } from './users.service';
+import { l } from '../logger';
 // TODO: Pending ADD  Swagger Document endpoints and request payload validators
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('login')
-   async login(@Body() loginPayload: LoginPayloadDTO): Promise<{ token: string }> {
-    try {
-      return await this.usersService.login(loginPayload);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  login(@Body() loginPayload: LoginPayloadDTO): Promise<{ token: string }>{
+   return this.usersService.login(loginPayload).catch((err)=>{ 
+     l.warn(`[UsersController] [login] ${err} --errors ${JSON.stringify(err.response.error)}`)
+     throw new HttpException({ message: err.response.message, error: err.response.error || err.response.message }, err.status)
+   })
+ }
+
 }
