0a2f4d5ab46ceeb4c613e5da0631dccda6315a47
CESAR ALVAREZ <calvarez@asastec.com>
2022-06-30 12:47:47 +0200

    delete map reference-price

--- src/management/services/packages-newblue.service.ts
+++ src/management/services/packages-newblue.service.ts
@@ -5,11 +5,11 @@ import { ManagementHttpService } from './management-http.service';
 
 export class ReferencePricesResponseDTO {
   data?: DataPriceDTO;
-  status?: number
+  status?: number;
 }
 export class DataPriceDTO {
   rows?: ProductPriceDTO[];
-  count?: number
+  count?: number;
 }
 export class ProductPriceDTO {
   origins?: OriginsDTO[];
@@ -21,26 +21,20 @@ export class OriginsDTO {
 export class DatesPricesDTO {
   date?: string;
   amount?: number;
-  status?:string
+  status?: string;
 }
 
 @Injectable()
 export class PackagesNewblueService {
   constructor(private readonly appConfigService: AppConfigService, private readonly managementHttpService: ManagementHttpService) {}
 
-  async postPackagesNewblueReferencePrices(data, headers?:HeadersDTO) {
-    const info = await this.managementHttpService.post<ReferencePricesResponseDTO>(`${this.appConfigService.BASE_URL}/packages-newblue/api/v1/referencePrices`, data, {
-      headers
-    });
-
-    info.data.rows.forEach((row)=>{
-       row.origins.forEach((origin)=>{
-        let newDates = origin.dates.filter((date)=> date?.status !== 'WL' && date?.status !== 'NA');
-        origin.dates = newDates;
-      })
-    });
-
-    return info;
-
+  async postPackagesNewblueReferencePrices(data, headers?: HeadersDTO) {
+    return this.managementHttpService.post<ReferencePricesResponseDTO>(
+      `${this.appConfigService.BASE_URL}/packages-newblue/api/v1/referencePrices`,
+      data,
+      {
+        headers,
+      },
+    );
   }
 }
