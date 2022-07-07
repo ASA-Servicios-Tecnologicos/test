5f2eff9679121393a6c74e6d34c30b18a233c21f
Osti Katriel <osti.barrozo.pe@cloudappi.net>
2022-07-07 17:20:54 -0500

    change name

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,19 +3,19 @@ import { logger } from '../logger';
 export function getCodeMethodType(methodType: string) {
   logger.info(`[utils] [getCodeMethodType] --methodType ${methodType}`);
 
-  let code: number;
+  let id: number;
   switch (methodType) {
     case 'CARD':
-      code = 4;
+      id = 4;
       break;
     case 'BANK_TRANSFER':
-      code = 2;
+      id = 2;
       break;
     default:
       //Bizum
-      code = 7; //Este id debe cambiar, dependiendo de la id que se genere en la base de datos
+      id = 7; //Este id debe cambiar, dependiendo de la id que se genere en la base de datos
       break;
   }
 
-  return code;
+  return id;
 }
